1ed6f8285aa8b4235a4ce900a2979dadd3ff3422
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-07-23 15:26:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,35 @@ class DenseLayer:
         self.output = np.dot(inputs, self.weights) + self.biases
         self.inputs = inputs
     
-    def backward(self, dvalues):
-        self.dinputs = dvalues.copy()
-        self.dinputs[self.inputs <= 0] = 0
+    def backward(self, dvalues: np.ndarray) -> None:
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
         
 class ActivationReLU:
     
-    def forward(self,inputs: np.ndarray) -> None:
-       self.output = np.maximum(0,inputs) 
+    def forward(self, inputs: np.ndarray) -> None:
+       self.output = np.maximum(0,inputs)
+       self.inputs = inputs
+
+    def backward(self, dvalues: np.ndarray) -> None:
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
         
 class ActivationSoftmax:
     
     def forward(self, inputs: np.ndarray) -> None:
+        self.inputs = inputs
         exp_max_input = np.exp(inputs - np.max(inputs, axis=1,keepdims=True))
         sum_input = np.sum(exp_max_input, axis=1, keepdims=True)
-        self.output = exp_max_input/sum_input
+        self.output = exp_max_input / sum_input
+
+    def backward(self, dvalues: np.ndarray) -> None:
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, -1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 
 
 class Loss:
@@ -48,6 +62,35 @@ class CategoricalCrossEntropy(Loss):
             confidences = np.sum(y_pred_clip*y_true, axis=1)
         
         return -np.log(confidences)
+
+    def backward(self, dvalues: np.ndarray, y_true: np.ndarray) -> None:
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
+class ActivationSoftmaxCrossEntropy():
+    def __init__(self):
+        self.activation = ActivationSoftmax()
+        self.loss = CategoricalCrossEntropy()
+
+    def forward(self, inputs: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+
+        return self.loss.calculate(self.output, y_true)
+
+    def backward(self, dvalues: np.ndarray, y_true: np.ndarray) -> None:
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
 
 def accuracy(y_pred: np.ndarray, y_true: np.ndarray) -> float:
     
